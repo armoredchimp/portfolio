54e9490741a614e8e539338f2369d2e2d168a0f5
armoredchimp <mattkasmir@gmail.com>
2024-03-02 20:11:29 -0800

    3/2

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const email = document.querySelector('.email');
 const copyIcon = document.querySelector('.copy-icon');
+const projLink = document.querySelector('.top-proj');
+const projStart = document.querySelector('.ac1');
+
+const certLink = document.querySelector('.top-certs');
+const certStart = document.querySelector('.ac3');
+
+const contactLink = document.querySelector('.top-contact');
+const contactStart = document.querySelector('.contact-header');
 
 copyIcon.addEventListener('click', () => {
   navigator.clipboard.writeText(email.textContent);
   console.log('Email address copied');
 });
+
+projLink.addEventListener('click', () => scrollProj());
+certLink.addEventListener('click', () => scrollCerts());
+contactLink.addEventListener('click', () => scrollContact());
+
+function scrollProj() {
+  projStart.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+  });
+}
+function scrollCerts() {
+  certStart.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+  });
+}
+function scrollContact() {
+  contactStart.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+  });
+}
